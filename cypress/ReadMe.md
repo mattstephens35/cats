9da46e0ed905d55c9094974eb8e18f29de7846e3
mattstephens35 <mattstephens35@gmail.com>
2024-01-14 22:10:46 -0600

    creating project

---
+++ cypress/ReadMe.md
@@ -0,0 +1,15 @@
+# Cats API tests
+
+This project contains tests for the Cats API located at https://api.api-ninjas.com/v1/cats
+
+## Installation
+
+`npm install`
+
+## Running tests
+
+`npx cypress run --headless`
+
+## Test Reports
+
+Test reports are stored in cypress/reports/
---
+++ cypress/e2e/cats-api.cy.js
@@ -0,0 +1,38 @@
+const apiKey = 'ncTBJBpHnBt15D/K38UgDA==XncB5bK0GjNYLtNg'
+
+describe('cats-api tests', () => {
+  before(function () {
+    cy.fixture('cats').then(function (data) {
+      this.data = data;
+    })
+  })
+  it('GET /cats by name', function () {
+    cy.request({
+        method: 'GET',
+        headers: { 'x-api-key': apiKey },
+        url: 'https://api.api-ninjas.com/v1/cats',
+        qs: {
+          "name": this.data.name
+        } 
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      const catBody = response.body[0];
+      expect(catBody.length).to.equal(this.data.length)
+      expect(catBody.origin).to.equal(this.data.origin)
+      expect(catBody.image_link).to.equal(this.data.image_link)
+      expect(catBody.family_friendly).to.equal(this.data.family_friendly)
+      expect(catBody.shedding).to.equal(this.data.shedding)
+      expect(catBody.general_health).to.equal(this.data.general_health)
+      expect(catBody.playfulness).to.equal(this.data.playfulness)
+      expect(catBody.children_friendly).to.equal(this.data.children_friendly)
+      expect(catBody.grooming).to.equal(this.data.grooming)
+      expect(catBody.intelligence).to.equal(this.data.intelligence)
+      expect(catBody.min_weight).to.equal(this.data.min_weight)
+      expect(catBody.max_weight).to.equal(this.data.max_weight)
+      expect(catBody.min_life_expectancy).to.equal(this.data.min_life_expectancy)
+      expect(catBody.max_life_expectancy).to.equal(this.data.max_life_expectancy)
+      expect(catBody.name).to.equal(this.data.name)
+     })
+  })
+})  
+
